b9cdded09ac2588034e89047de2c38fbca1be8fe
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-03 18:13:27 -0300

    novo método public WebsiteDomain findByServerName(String serverName)

--- source/atius/src/main/java/br/ufpa/ctic/atius/persistence/WebsiteDomainDAO.java
+++ source/atius/src/main/java/br/ufpa/ctic/atius/persistence/WebsiteDomainDAO.java
@@ -14,17 +14,32 @@ public class WebsiteDomainDAO extends LDAPCrud<WebsiteDomain, String> {
 
 	private static final long serialVersionUID = 1L;
 
+	private WebsiteDomain entry2websiteDomain(Map<String, String[]> entry) {
+		WebsiteDomain websiteDomain = new WebsiteDomain();
+		websiteDomain.setCn(entry.get("cn")[0]);
+		websiteDomain.setServerName(entry.get("serverName")[0]);
+		websiteDomain.setWebsiteCategory(entry.get("websiteCategory")[0]);
+		websiteDomain.setWebsiteProfile(entry.get("websiteProfile")[0]);
+		return websiteDomain;
+	}
+
 	public List<WebsiteDomain> findAll() {
 		List<WebsiteDomain> websiteDomains = new ArrayList<WebsiteDomain>();
-		Collection<Map<String, String[]>> entries = getEntryManager().createQuery("objectClass=websiteDomain").getResultCollection();
+		Collection<Map<String, String[]>> entries = getEntryManager().createQuery("objectClass=websiteDomain")
+				.getResultCollection();
 		for (Map<String, String[]> entry : entries) {
-			WebsiteDomain websiteDomain = new WebsiteDomain();
-			websiteDomain.setCn(entry.get("cn")[0]);
-			websiteDomain.setServerName(entry.get("serverName")[0]);
-			websiteDomain.setWebsiteCategory(entry.get("websiteCategory")[0]);
-			websiteDomain.setWebsiteProfile(entry.get("websiteProfile")[0]);
-			websiteDomains.add(websiteDomain);
+			websiteDomains.add(entry2websiteDomain(entry));
 		}
 		return websiteDomains;
+	}
+
+	public WebsiteDomain findByServerName(String serverName) {
+		WebsiteDomain websiteDomain = new WebsiteDomain();
+		Map<String, String[]> entry = getEntryManager().createQuery(
+				"(&(objectClass=websiteDomain)(serverName=" + serverName + "))").getSingleResult();
+		if (entry.size() != 0) {
+			websiteDomain = entry2websiteDomain(entry);
+		}
+		return websiteDomain;
 	}
 }
