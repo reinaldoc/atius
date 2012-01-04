3ac1663505535a95e45fcaeb77629bd76b890236
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-04 18:20:11 -0300

    nova tag atius:dialogButtons com o uso de jquery.qtip

---
+++ source/atius/src/main/webapp/resources/javascript/atius.js
@@ -0,0 +1,103 @@
+/**
+ * saveAndCloseButtons methods
+ * 
+ * @author J. Riba Cruz - jribacruz@gmail.com
+ */
+function id(_id) {
+	return ('#' + _id.replace(':', '\\:'));
+}
+
+function getDefaultBar(componentId) {
+	return jQuery(id(componentId + ":default-bar"));
+}
+
+function getSuccessBar(componentId) {
+	return jQuery(id(componentId + ":success-bar"));
+}
+
+function getSavingBar(componentId) {
+	return jQuery(id(componentId + ":saving-bar"));
+}
+
+function getErrorBar(componentId) {
+	return jQuery(id(componentId + ":error-bar"));
+}
+
+function startSave(dialog, componentId) {
+	getDefaultBar(componentId).hide();
+	getErrorBar(componentId).hide();
+	getSuccessBar(componentId).hide();
+	getSavingBar(componentId).show();
+	blockDialogInputs(dialog);
+}
+
+function blockDialogInputs(dialog) {
+	jQuery('#' + dialog + ' .dialog-body input:not(.sds-readonly)').attr(
+			'readonly', 'readonly');
+	jQuery('#' + dialog + ' .dialog-body textarea:not(.sds-readonly)').attr(
+			'readonly', 'readonly');
+}
+
+function unBlockDialogInputs(dialog) {
+	jQuery('#' + dialog + ' .dialog-body input:not(.sds-readonly)').removeAttr(
+			'readonly');
+	jQuery('#' + dialog + ' .dialog-body textarea:not(.sds-readonly)')
+			.removeAttr('readonly');
+}
+
+function showNotificationClass(componentId, qtipStyleClass) {
+	mId = id(componentId + ':messages');
+	lId = id(componentId + ':message-link');
+
+	jQuery(lId).qtip({
+
+		content : {
+
+			text : jQuery(mId)
+		},
+		events : {
+			show : function(event, api) {
+				jQuery(this).qtip('api').set('content.text', jQuery(mId));
+				jQuery(mId).show();
+			}
+		},
+		hide : {
+			event : 'mouseleave'
+		},
+		position : {
+			my : 'bottom right',
+			at : 'top center'
+		},
+		style : {
+			classes : qtipStyleClass + ' ui-tooltip-shadow ui-tooltip-rounded'
+		}
+	});
+	jQuery(lId).qtip('api').show();
+}
+
+function showNotification(componentId) {
+	getErrorBar(componentId).show();
+	showNotificationClass(componentId, 'ui-tooltip-green');
+}
+
+function completeSave(xhr, status, args, dialog, componentId) {
+	if (args.validationFailed || args.appValidationFailed) {
+
+		getDefaultBar(componentId).show();
+		getErrorBar(componentId).show();
+
+		unBlockDialogInputs(dialog);
+		showNotificationClass(componentId, 'ui-tooltip-red');
+
+		getSuccessBar(componentId).hide();
+		getSavingBar(componentId).hide();
+
+	} else {
+
+		getDefaultBar(componentId).hide();
+		getErrorBar(componentId).hide();
+		getSavingBar(componentId).hide();
+
+		getSuccessBar(componentId).show();
+	}
+}
