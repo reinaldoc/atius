c4ba6130872c596c10c2496629fe3cec5c3f7c99
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-04 18:26:01 -0300

    método para seleção dos resposáveis pelo site

--- source/atius/src/main/java/br/ufpa/ctic/atius/view/edit/WebsiteDomainEditMB.java
+++ source/atius/src/main/java/br/ufpa/ctic/atius/view/edit/WebsiteDomainEditMB.java
@@ -1,5 +1,7 @@
 package br.ufpa.ctic.atius.view.edit;
 
+import java.util.List;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
@@ -7,6 +9,7 @@ import javax.inject.Inject;
 import br.gov.frameworkdemoiselle.stereotype.ViewController;
 import br.gov.frameworkdemoiselle.template.AbstractEditPageBean;
 import br.ufpa.ctic.atius.business.WebsiteDomainBC;
+import br.ufpa.ctic.atius.domain.InetOrgPerson;
 import br.ufpa.ctic.atius.domain.WebsiteDomain;
 
 @ViewController
@@ -28,12 +31,18 @@ public class WebsiteDomainEditMB extends AbstractEditPageBean<WebsiteDomain, Str
 
 	@Override
 	public String insert() {
+		System.out.println("========> Insert() "+getBean().getServerName());
+		if (!bc.domainAvailable(getBean().getServerName())) {
+			facesContext.addMessage("sites", new FacesMessage("Domínio indisponível, escolha outro."));
+			facesContext.validationFailed();
+		}
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String update() {
+		System.out.println("========> Update()");
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -46,11 +55,16 @@ public class WebsiteDomainEditMB extends AbstractEditPageBean<WebsiteDomain, Str
 
 	public String domainAvailable() {
 		if (bc.domainAvailable(getBean().getServerName()))
-			facesContext.addMessage("growl", new FacesMessage("O domínio " + getBean().getServerName()
+			facesContext.addMessage("sites", new FacesMessage("O domínio " + getBean().getServerName()
 					+ " está disponível"));
-		else
-			facesContext.addMessage("growl", new FacesMessage("Domínio indisponível, escolha outro."));
+		else {
+			facesContext.addMessage("sites", new FacesMessage("Domínio indisponível, escolha outro."));
+			facesContext.validationFailed();
+		}
 		return null;
 	}
 
+	public List<InetOrgPerson> findPerson(String search) {
+		return bc.findPerson(search);
+	}
 }
