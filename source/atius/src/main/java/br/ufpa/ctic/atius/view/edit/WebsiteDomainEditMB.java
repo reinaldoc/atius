65bc18bef87646b6a9153aa56c0aa18ad1ea7623
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-08 03:14:57 -0300

    refatoração para adequar a nova AbstractEditPageBean

--- source/atius/src/main/java/br/ufpa/ctic/atius/view/edit/WebsiteDomainEditMB.java
+++ source/atius/src/main/java/br/ufpa/ctic/atius/view/edit/WebsiteDomainEditMB.java
@@ -2,12 +2,10 @@ package br.ufpa.ctic.atius.view.edit;
 
 import java.util.List;
 
-import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
 
 import br.gov.frameworkdemoiselle.message.DefaultMessage;
 import br.gov.frameworkdemoiselle.stereotype.ViewController;
-import br.gov.frameworkdemoiselle.template.AbstractEditPageBean;
 import br.gov.frameworkdemoiselle.util.Faces;
 import br.ufpa.ctic.atius.business.WebsiteDomainBC;
 import br.ufpa.ctic.atius.domain.InetOrgPerson;
@@ -21,9 +19,15 @@ public class WebsiteDomainEditMB extends AbstractEditPageBean<WebsiteDomain, Str
 	@Inject
 	private WebsiteDomainBC bc;
 
-	@Override
 	public String delete() {
-		bc.delete(getId());
+		bc.delete(getBean().getServerName());
+		Faces.addMessage(new DefaultMessage("Domínio " + getBean().getServerName() + " excluído com sucesso."));
+		return null;
+	}
+
+	public String disable() {
+		// bc.disable(getBean().getServerName());
+		Faces.addMessage(new DefaultMessage("Domínio " + getBean().getServerName() + " desabilitado com sucesso."));
 		return null;
 	}
 
@@ -37,61 +41,16 @@ public class WebsiteDomainEditMB extends AbstractEditPageBean<WebsiteDomain, Str
 		return null;
 	}
 
-	@Override
 	public String update() {
 		System.out.println("==========> update()");
-		//bc.update(getBean());
+		// bc.update(getBean());
 		return null;
-	}
-
-	@Override
-	protected void handleLoad() {
-		// TODO Auto-generated method stub
-
 	}
 
 	public List<InetOrgPerson> findPerson(String search) {
 		return bc.findPerson(search);
 	}
 
-	/*
-	 * chamada a partir do commandButton para adição de um registro. O método
-	 * Faces.resetValidation() limpará o estado da validação de todos os inputs
-	 * da visão.
-	 */
-	public String create() {
-		setId(null);
-		setBean(new WebsiteDomain());
-		Faces.resetValidation();
-		return null;
-	}
-
-	/*
-	 * chamada a partir do commandButton que carrega para edição um registro. Se
-	 * o método resetParentFormValidation() foi chamado a patir do mesmo botao
-	 * via actionListener não é necessário chamar Faces.resetValidation() aqui;
-	 */
-	public String loadByServerName(String serverName) {
-		setId(serverName);
-		setBean(bc.load(serverName));
-		Faces.resetValidation();
-		return null;
-	}
-
-	/*
-	 * OPCIONALMENTE: chamado a partir de um actionListener a partir de um
-	 * commandButton dentro do mesmo form. O método
-	 * Faces.resetParentFormValidation limpará o estado da validação de cada
-	 * input SOMENTE DESTE form. Logo é mais perfomatico que o
-	 * Faces.resetValidation();
-	 */
-	public void resetParentFormValidation(ActionEvent ae) {
-		Faces.resetParentFormValidation(ae.getComponent());
-	}
-
-	/*
-	 * chamado a
-	 */
 	public String domainAvailable() {
 		if (bc.domainAvailable(getBean().getServerName()))
 			Faces.addMessage(new DefaultMessage("O domínio " + getBean().getServerName() + " está disponível"));
@@ -100,6 +59,13 @@ public class WebsiteDomainEditMB extends AbstractEditPageBean<WebsiteDomain, Str
 			Faces.validationFailed();
 		}
 		return null;
+	}
+
+	@Override
+	protected WebsiteDomain loadBean(String serverName) {
+		System.out.println("===> handleLoad()");
+		// TODO Auto-generated method stub
+		return bc.load(serverName);
 	}
 
 }
