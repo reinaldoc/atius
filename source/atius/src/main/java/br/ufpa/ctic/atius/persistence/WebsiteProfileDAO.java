5b50cd6b020cef5dbcc56e00eb152eec6be74ca5
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-09 00:03:42 -0300

    adição do método load() e ajustes menores

--- source/atius/src/main/java/br/ufpa/ctic/atius/persistence/WebsiteProfileDAO.java
+++ source/atius/src/main/java/br/ufpa/ctic/atius/persistence/WebsiteProfileDAO.java
@@ -5,8 +5,9 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import br.gov.frameworkdemoiselle.ldap.template.LDAPCrud;
 import br.gov.frameworkdemoiselle.stereotype.PersistenceController;
-import br.gov.frameworkdemoiselle.template.LDAPCrud;
+import br.gov.frameworkdemoiselle.util.StringUtils;
 import br.ufpa.ctic.atius.domain.WebsiteProfile;
 
 @PersistenceController
@@ -16,6 +17,10 @@ public class WebsiteProfileDAO extends LDAPCrud<WebsiteProfile, String> {
 
 	private WebsiteProfile entry2websiteProfile(Map<String, String[]> entry) {
 		WebsiteProfile websiteProfile = new WebsiteProfile();
+		if (entry.size() == 0)
+			return websiteProfile;
+		websiteProfile.setDn(entry.get("dn")[0]);
+		websiteProfile.setObjectClass(entry.get("objectClass"));
 		websiteProfile.setName(entry.get("cn")[0]);
 		websiteProfile.setWebserverName(entry.get("webserverName")[0]);
 		websiteProfile.setSchemaserverName(entry.get("schemaserverName")[0]);
@@ -33,6 +38,16 @@ public class WebsiteProfileDAO extends LDAPCrud<WebsiteProfile, String> {
 
 	public List<WebsiteProfile> findAll() {
 		return findBySearchFilter("objectClass=websiteProfile");
+	}
+
+	public WebsiteProfile load(String profileName) {
+		WebsiteProfile websiteProfile = new WebsiteProfile();
+		if (!StringUtils.isBlank(profileName)) {
+			Map<String, String[]> entry = getEntryManager().createQuery(
+					String.format("(&(objectClass=websiteProfile)(cn=%s))", profileName)).getSingleResult();
+			websiteProfile = entry2websiteProfile(entry);
+		}
+		return websiteProfile;
 	}
 
 }
