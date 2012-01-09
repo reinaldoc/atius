ca6aaf3dc66599dcd3b0fb869a997488aa1c4af7
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-09 18:36:09 -0300

    método para normalizar primeira letra maiuscula dos nomes.

--- source/atius/src/main/java/br/gov/frameworkdemoiselle/util/StringUtils.java
+++ source/atius/src/main/java/br/gov/frameworkdemoiselle/util/StringUtils.java
@@ -1,5 +1,6 @@
 package br.gov.frameworkdemoiselle.util;
 
+
 public class StringUtils extends org.apache.commons.lang.StringUtils {
 
 	public static String null2empty(String value) {
@@ -7,4 +8,18 @@ public class StringUtils extends org.apache.commons.lang.StringUtils {
 			return "";
 		return value;
 	}
+
+	@SuppressWarnings("deprecation")
+	public static String capitalizeBr(String str) {
+		String strTmp = capitaliseAllWords(str.toLowerCase());
+		strTmp = strTmp.replaceAll(" Da ", " da ");
+		strTmp = strTmp.replaceAll(" Das ", " das ");
+		strTmp = strTmp.replaceAll(" Dos ", " dos ");
+		strTmp = strTmp.replaceAll(" De ", " de ");
+		strTmp = strTmp.replaceAll(" Do ", " do ");
+		strTmp = strTmp.replaceAll(" Du ", " du ");
+		strTmp = strTmp.replaceAll(" E ", " e ");
+		return strTmp;
+	}
+
 }
