c4c23e3c3b4f2cc5fb94e2523f88fc8593d6ff41
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-09 19:08:38 -0300

    ManageBean de acesso a valores comuns da aplicaçãp

---
+++ source/atius/src/main/java/br/ufpa/ctic/atius/view/application/ProxyMB.java
@@ -0,0 +1,35 @@
+package br.ufpa.ctic.atius.view.application;
+
+import java.io.Serializable;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.gov.frameworkdemoiselle.internal.configuration.PaginationConfig;
+import br.gov.frameworkdemoiselle.security.SecurityContext;
+import br.gov.frameworkdemoiselle.util.StringUtils;
+
+@Named
+public class ProxyMB implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private PaginationConfig paginationConfig;
+
+	@Inject
+	private SecurityContext securityContext;
+
+	public PaginationConfig getPaginationConfig() {
+		return paginationConfig;
+	}
+
+	public void setPaginationConfig(PaginationConfig paginationConfig) {
+		this.paginationConfig = paginationConfig;
+	}
+
+	public String getUserNameProperCase() {
+		return StringUtils.capitalizeBr(this.securityContext.getUser().getId().toLowerCase());
+	}
+
+}
