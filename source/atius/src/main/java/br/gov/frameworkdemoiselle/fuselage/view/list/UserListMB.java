145122b9bfdc053711d77178a248ce807179a27b
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-11 00:51:41 -0300

    implementação opção de gerar PDF (relatorio)

--- source/atius/src/main/java/br/gov/frameworkdemoiselle/fuselage/view/list/UserListMB.java
+++ source/atius/src/main/java/br/gov/frameworkdemoiselle/fuselage/view/list/UserListMB.java
@@ -1,15 +1,21 @@
 package br.gov.frameworkdemoiselle.fuselage.view.list;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import br.gov.frameworkdemoiselle.fuselage.business.UserBC;
 import br.gov.frameworkdemoiselle.fuselage.domain.SecurityUser;
+import br.gov.frameworkdemoiselle.report.Report;
+import br.gov.frameworkdemoiselle.report.Type;
+import br.gov.frameworkdemoiselle.report.annotation.Path;
 import br.gov.frameworkdemoiselle.stereotype.ViewController;
 import br.gov.frameworkdemoiselle.template.AbstractListPageBean;
 import br.gov.frameworkdemoiselle.transaction.Transactional;
+import br.gov.frameworkdemoiselle.util.FileRenderer;
 
 @ViewController
 public class UserListMB extends AbstractListPageBean<SecurityUser, Long> {
@@ -17,6 +23,13 @@ public class UserListMB extends AbstractListPageBean<SecurityUser, Long> {
 
 	@Inject
 	private UserBC bc;
+
+	@Inject
+	@Path("reports/SecurityUser.jrxml")
+	private Report report;
+	
+	@Inject
+	private FileRenderer renderer;
 
 	@PostConstruct
 	public void init() {
@@ -32,6 +45,13 @@ public class UserListMB extends AbstractListPageBean<SecurityUser, Long> {
 	public String deleteSelection() {
 		bc.delete(getSelectedList());
 		clearSelection();
+		return null;
+	}
+
+	public String print() {
+		Map<String, Object> param = new HashMap<String, Object>();
+		byte[] buffer = report.export(getResultList(), param, Type.PDF);
+		this.renderer.render(buffer, FileRenderer.ContentType.PDF, "relatorio.pdf");
 		return null;
 	}
 
