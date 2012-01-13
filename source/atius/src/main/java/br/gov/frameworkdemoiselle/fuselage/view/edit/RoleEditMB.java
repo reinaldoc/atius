8dde6630dd9d6924c32d273d892316f659f57866
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-13 02:46:09 -0300

    add/del resource from a role

--- source/atius/src/main/java/br/gov/frameworkdemoiselle/fuselage/view/edit/RoleEditMB.java
+++ source/atius/src/main/java/br/gov/frameworkdemoiselle/fuselage/view/edit/RoleEditMB.java
@@ -3,9 +3,12 @@ package br.gov.frameworkdemoiselle.fuselage.view.edit;
 import javax.inject.Inject;
 
 import br.gov.frameworkdemoiselle.fuselage.business.RoleBC;
+import br.gov.frameworkdemoiselle.fuselage.domain.SecurityResource;
 import br.gov.frameworkdemoiselle.fuselage.domain.SecurityRole;
+import br.gov.frameworkdemoiselle.message.DefaultMessage;
 import br.gov.frameworkdemoiselle.stereotype.ViewController;
 import br.gov.frameworkdemoiselle.template.AbstractEditPageBean;
+import br.gov.frameworkdemoiselle.util.Faces;
 
 @ViewController
 public class RoleEditMB extends AbstractEditPageBean<SecurityRole, Long> {
@@ -35,6 +38,32 @@ public class RoleEditMB extends AbstractEditPageBean<SecurityRole, Long> {
 
 	@Override
 	public SecurityRole load(Long id) {
+		return null;
+	}
+
+	public String addResource(SecurityResource resource) {
+		System.out.println("==> add " + resource.getName() + ":" + resource.getValue());
+		if (getBean().getResource() != null)
+			getBean().getResource().add(resource);
+		else
+			Faces.validationFailed(new DefaultMessage("Oooopppsss... a operação não pode ser realizada"));
+		return null;
+	}
+
+	public String delResource(SecurityResource resource) {
+		System.out.println("==> del " + resource.getName() + ":" + resource.getValue());
+		if (getBean().getResource() != null) {
+			for (int i = 0; i < getBean().getResource().size(); i++) {
+				SecurityResource securityResource = getBean().getResource().get(i);
+				System.out.println("==> compare " + securityResource.getName() + ":" + resource.getName());
+				if (securityResource == resource) {
+					System.out.println("==> compare " + securityResource.getName() + ":" + resource.getName() + " true");
+					getBean().getResource().remove(i);
+					i--;
+				}
+			}
+		} else
+			Faces.validationFailed(new DefaultMessage("Oooopppsss... a operação não pode ser realizada"));
 		return null;
 	}
 
