9e3a6e981581c0b9b57de7e6491ea7591f8b6315
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-17 02:10:09 -0300

    implementação da associação de roles a perfis no dialog de edição de perfil.

--- source/atius/src/main/java/br/gov/frameworkdemoiselle/fuselage/view/edit/ProfileEditMB.java
+++ source/atius/src/main/java/br/gov/frameworkdemoiselle/fuselage/view/edit/ProfileEditMB.java
@@ -1,9 +1,15 @@
 package br.gov.frameworkdemoiselle.fuselage.view.edit;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import br.gov.frameworkdemoiselle.fuselage.business.ProfileBC;
 import br.gov.frameworkdemoiselle.fuselage.domain.SecurityProfile;
+import br.gov.frameworkdemoiselle.fuselage.domain.SecurityResource;
+import br.gov.frameworkdemoiselle.fuselage.domain.SecurityRole;
 import br.gov.frameworkdemoiselle.stereotype.ViewController;
 import br.gov.frameworkdemoiselle.template.AbstractEditPageBean;
 
@@ -35,7 +41,53 @@ public class ProfileEditMB extends AbstractEditPageBean<SecurityProfile, Long> {
 
 	@Override
 	public SecurityProfile load(Long id) {
-		return null;
+		return bc.load(id);
+	}
+
+	public List<Long> resourcePrio() {
+		List<Long> priorities = new ArrayList<Long>();
+		for (int i = 1; i < 101; i++)
+			priorities.add(new Long(i));
+		return priorities;
+	}
+
+	/**
+	 * Get all SecurityResources for selectOneMenu to select welcome page
+	 * 
+	 * @return list of all SecurityResources
+	 */
+	public List<SecurityResource> getResourceList() {
+		return bc.getResources();
+	}
+
+	/**
+	 * Get all SecurityRoles for datatable
+	 * 
+	 * @return list of all SecurityRoles
+	 */
+	public List<SecurityRole> getRoleList() {
+		return bc.getRoles();
+	}
+
+	/**
+	 * Get SecurityRoles from current bean as array for datatable selection
+	 * 
+	 * @return array of bean SecurityRoles
+	 */
+	public SecurityRole[] getRoleArray() {
+		if (getBean().getRoles() == null)
+			return null;
+		return getBean().getRoles().toArray(new SecurityRole[0]);
+	}
+
+	/**
+	 * Set SecurityRoles on current bean from datatable selection array
+	 * 
+	 * @param roles
+	 *            array of SecurityRoles to set current bean
+	 */
+	public void setRoleArray(SecurityRole[] roles) {
+		getBean().setRoles(Arrays.asList(roles));
 	}
 
 }
