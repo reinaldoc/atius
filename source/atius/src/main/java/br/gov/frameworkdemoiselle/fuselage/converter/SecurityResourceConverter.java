f441e27691f6314ae3272ac9be21c837cad0ab28
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-17 13:30:58 -0300

    implementação do SecurityResourceConverter

--- source/atius/src/main/java/br/gov/frameworkdemoiselle/fuselage/converter/SecurityResourceConverter.java
+++ source/atius/src/main/java/br/gov/frameworkdemoiselle/fuselage/converter/SecurityResourceConverter.java
@@ -5,35 +5,30 @@ import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
+import br.gov.frameworkdemoiselle.fuselage.business.ResourceBC;
 import br.gov.frameworkdemoiselle.fuselage.domain.SecurityResource;
+import br.gov.frameworkdemoiselle.util.Beans;
 
 @FacesConverter("securityResource")
 public class SecurityResourceConverter implements Converter {
 
 	public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
-		System.out.println("o======> " + value);
 		if (value.trim().isEmpty()) {
-			SecurityResource securityResource = new SecurityResource();
-			securityResource.setId(new Long(0));
-			securityResource.setName("xxx");
-			securityResource.setValue("xxx");
-			return securityResource;
+			return null;
 		} else {
-			SecurityResource securityResource = new SecurityResource();
-			securityResource.setId(new Long(value));
-			securityResource.setName("xxx");
-			securityResource.setValue("xxx");
-			return securityResource;
+			return resourceBC().load(new Long(value));
 		}
 	}
 
-	public String getAsString(FacesContext facesContext, UIComponent component, Object value) {
-		System.out.println("s======> " + value);
-		if (value == null || value.equals("")) {
+	public String getAsString(FacesContext facesContext, UIComponent component, Object securityResource) {
+		if (securityResource instanceof SecurityResource)
+			return ((SecurityResource) securityResource).getId().toString();
+		else
 			return "";
-		} else {
-			return ((SecurityResource) value).getId().toString();
-		}
+	}
+
+	private ResourceBC resourceBC() {
+		return Beans.getReference(ResourceBC.class);
 	}
 
 }
