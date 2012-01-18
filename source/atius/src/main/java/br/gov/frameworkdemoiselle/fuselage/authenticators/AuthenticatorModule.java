b554a713d89f08589e7d9fbd3bd09dd910be897f
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-18 02:30:42 -0300

    ctrl+alt+f

--- source/atius/src/main/java/br/gov/frameworkdemoiselle/fuselage/authenticators/AuthenticatorModule.java
+++ source/atius/src/main/java/br/gov/frameworkdemoiselle/fuselage/authenticators/AuthenticatorModule.java
@@ -1,8 +1,9 @@
 package br.gov.frameworkdemoiselle.fuselage.authenticators;
 
 public interface AuthenticatorModule {
+
 	public boolean authenticate(String username, String password);
-	
+
 	public AuthenticatorResults getResults();
 
 }
