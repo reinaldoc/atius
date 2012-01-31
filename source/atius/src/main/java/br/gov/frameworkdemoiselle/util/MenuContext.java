bb0b24073ce7ed970ce68196baaf092fbaac00b8
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-31 17:56:52 -0300

    estrutura para modulo DHCP

--- source/atius/src/main/java/br/gov/frameworkdemoiselle/util/MenuContext.java
+++ source/atius/src/main/java/br/gov/frameworkdemoiselle/util/MenuContext.java
@@ -22,6 +22,7 @@ public class MenuContext extends AbstractMenuContext {
 	public MenuContext() {
 		select("MenuTop", "Sites");
 		select("MenuEmail", "Institucional");
+		select("MenuDhcp", "x");
 		select("Security", "User");
 	}
 
