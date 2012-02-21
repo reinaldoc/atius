0d7ce38c035e627db8107be5b6daf68b1fb3f7ed
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-02-21 13:17:06 -0300

    inserir DhcpContainerDN configurado se não existir

--- source/atius/src/main/java/br/ufpa/ctic/atius/dhcp/business/DhcpContainerBC.java
+++ source/atius/src/main/java/br/ufpa/ctic/atius/dhcp/business/DhcpContainerBC.java
@@ -1,9 +1,12 @@
 package br.ufpa.ctic.atius.dhcp.business;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import br.gov.frameworkdemoiselle.stereotype.BusinessController;
 import br.gov.frameworkdemoiselle.template.contrib.DelegateCrud;
+import br.gov.frameworkdemoiselle.util.contrib.Strings;
 import br.ufpa.ctic.atius.configuration.DhcpConfig;
 import br.ufpa.ctic.atius.dhcp.domain.DhcpContainer;
 import br.ufpa.ctic.atius.dhcp.persistence.DhcpContainerDAO;
@@ -18,11 +21,28 @@ public class DhcpContainerBC extends DelegateCrud<DhcpContainer, String, DhcpCon
 
 	public String getPrimaryDhcpServer() {
 		getQueryConfig().setGeneric(dhcpConfig.getDhcpContainerDN());
-		try {
-			return findAll().get(0).getDhcpPrimaryServer();
-		} catch (Exception e) {
+		List<DhcpContainer> dhcpContainers = findAll();
+		if (dhcpContainers.size() == 0) {
+			DhcpContainer dhcpContainer = new DhcpContainer();
+			dhcpContainer.setDn(dhcpConfig.getDhcpContainerDN());
+			dhcpContainer.setCn(Strings.substringAfter(Strings.substringBefore(dhcpConfig.getDhcpContainerDN(), ","), "="));
+			try {
+				insert(dhcpContainer);
+			} catch (RuntimeException e) {
+				e.printStackTrace();
+			}
 			return null;
 		}
+		return findAll().get(0).getDhcpPrimaryServer();
 	}
 
+	public void selectPrimaryDhcpServer(String dhcpServerName) {
+		getQueryConfig().setGeneric(dhcpConfig.getDhcpContainerDN());
+		List<DhcpContainer> dhcpContainers = findAll();
+		if (dhcpContainers.size() != 0) {
+			dhcpContainers.get(0).setDhcpPrimaryServer(dhcpServerName);
+			update(dhcpContainers.get(0));
+		}
+	}
+	
 }
