30f96a9ea43a92ee389fbc24df3a87182517d5c5
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2013-03-06 23:32:01 -0300

    new dialog to select primary (default) dhcp server

--- source/atius-dhcp/src/main/java/br/com/atius/dhcp/view/list/DhcpServerListMB.java
+++ source/atius-dhcp/src/main/java/br/com/atius/dhcp/view/list/DhcpServerListMB.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import br.com.atius.dhcp.business.DhcpContainerBC;
 import br.com.atius.dhcp.business.DhcpServerBC;
 import br.com.atius.dhcp.domain.DhcpServer;
 import br.com.atius.dhcp.view.edit.DhcpServiceEditMB;
+import br.gov.frameworkdemoiselle.message.SeverityType;
 import br.gov.frameworkdemoiselle.query.contrib.QueryConfig;
 import br.gov.frameworkdemoiselle.stereotype.ViewController;
 import br.gov.frameworkdemoiselle.template.contrib.AbstractListPageBean;
 import br.gov.frameworkdemoiselle.util.contrib.Faces;
+import br.gov.frameworkdemoiselle.util.contrib.Strings;
 
 @ViewController
 public class DhcpServerListMB extends AbstractListPageBean<DhcpServer, String> {
@@ -20,6 +23,11 @@ public class DhcpServerListMB extends AbstractListPageBean<DhcpServer, String> {
 	@Inject
 	private DhcpServerBC bc;
 
+	@Inject
+	private DhcpContainerBC dhcpContainerBC;
+
+	private String defaultDhcpServer;
+
 	@Override
 	protected List<DhcpServer> handleResultList(QueryConfig<DhcpServer> queryConfig) {
 		queryConfig.setGeneric(bc.getDhcpContainerDN());
@@ -27,13 +35,35 @@ public class DhcpServerListMB extends AbstractListPageBean<DhcpServer, String> {
 	}
 
 	public void selectDhcpServer(DhcpServer dhcpServer) {
-		if (dhcpServer.equals(bc.getDhcpServer()))
-			bc.selectPrimaryDhcpServer(dhcpServer.getCn());
 		bc.selectDhcpServer(dhcpServer);
 		Faces.getManagedProperty("#{dhcpSharedNetworkListMB}", DhcpSharedNetworkListMB.class).list();
 		Faces.getManagedProperty("#{dhcpSharedNetworkListMB}", DhcpSharedNetworkListMB.class).init();
 		Faces.getManagedProperty("#{dhcpServiceEditMB}", DhcpServiceEditMB.class).init();
 		Faces.getManagedProperty("#{dhcpSubnetListMB}", DhcpSubnetListMB.class).list();
+	}
+
+	public String getDefaultDhcpServer() {
+		return dhcpContainerBC.getPrimaryDhcpServer();
+	}
+
+	public void setDefaultDhcpServer(String defaultDhcpServer) {
+		this.defaultDhcpServer = defaultDhcpServer;
+	}
+
+	public void updateDefaultDhcpServer() {
+		if (Strings.isBlank(defaultDhcpServer)) {
+			Faces.validationFailed();
+			Faces.addI18nMessage("atius.dhcp.server.defaultserver.empty", SeverityType.ERROR);
+			return;
+		}
+		
+		try {
+			bc.selectPrimaryDhcpServer(defaultDhcpServer);
+		} catch (RuntimeException e) {
+			Faces.validationFailed();
+			Faces.addI18nMessage("atius.dhcp.server.defaultserver.failed", SeverityType.ERROR);
+		}
+
 	}
 
 }
