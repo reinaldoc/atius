7a2606396075508d95b85166e7cef5042b5015dd
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2013-05-23 15:22:25 -0300

    new dialog to add ServiceArea

---
+++ source/atius-services/src/main/java/br/com/atius/services/view/edit/ServiceAreaEditMB.java
@@ -0,0 +1,64 @@
+package br.com.atius.services.view.edit;
+
+import javax.inject.Inject;
+
+import br.com.atius.services.business.ServiceAreaBC;
+import br.com.atius.services.domain.ServiceArea;
+import br.gov.frameworkdemoiselle.message.SeverityType;
+import br.gov.frameworkdemoiselle.stereotype.ViewController;
+import br.gov.frameworkdemoiselle.template.contrib.AbstractEditPageBean;
+import br.gov.frameworkdemoiselle.util.contrib.Faces;
+
+@ViewController
+public class ServiceAreaEditMB extends AbstractEditPageBean<ServiceArea, Integer> {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private ServiceAreaBC bc;
+
+	@Override
+	public String insert() {
+		try {
+			bc.insert(getBean());
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+			Faces.validationFailed();
+			Faces.addI18nMessage("atius.error.generic", SeverityType.ERROR);
+		}
+		return null;
+	}
+
+	@Override
+	public String update() {
+		try {
+			bc.update(getBean());
+		} catch (RuntimeException e) {			Faces.validationFailed();
+			Faces.addI18nMessage("atius.error.generic", SeverityType.ERROR);
+		}
+		return null;
+	}
+
+	@Override
+	public String delete() {
+		try {
+			bc.delete(getBean().getId());
+		} catch (RuntimeException e) {
+			Faces.validationFailed();
+			Faces.addI18nMessage("atius.error.generic", SeverityType.ERROR);
+		}
+		return null;
+	}
+
+	@Override
+	public ServiceArea load(Integer id) {
+		try {
+			return bc.load(id);
+		} catch (RuntimeException e) {
+			Faces.validationFailed();
+			Faces.addI18nMessage("atius.error.generic", SeverityType.ERROR);
+		}
+		return new ServiceArea();
+	}
+
+}
