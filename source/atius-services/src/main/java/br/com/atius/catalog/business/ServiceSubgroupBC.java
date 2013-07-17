d49c0bac5063fcacefca13400fda72870f62bb8b
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2013-07-17 15:29:58 -0300

    update business

--- source/atius-services/src/main/java/br/com/atius/catalog/business/ServiceSubgroupBC.java
+++ source/atius-services/src/main/java/br/com/atius/catalog/business/ServiceSubgroupBC.java
@@ -3,29 +3,21 @@ package br.com.atius.catalog.business;
 import javax.inject.Inject;
 
 import br.com.atius.catalog.domain.ServiceGroup;
-import br.com.atius.catalog.domain.ServiceItem;
 import br.com.atius.catalog.domain.ServiceSubgroup;
-import br.com.atius.catalog.persistence.ServiceItemDAO;
+import br.com.atius.catalog.persistence.ServiceSubgroupDAO;
 import br.gov.frameworkdemoiselle.stereotype.BusinessController;
 import br.gov.frameworkdemoiselle.template.DelegateCrud;
 
 @BusinessController
-public class ServiceItemBC extends DelegateCrud<ServiceItem, Integer, ServiceItemDAO> {
+public class ServiceSubgroupBC extends DelegateCrud<ServiceSubgroup, Integer, ServiceSubgroupDAO> {
 
 	private static final long serialVersionUID = 1L;
 
 	@Inject
 	private ServiceGroupBC serviceGroupBC;
 
-	@Inject
-	private ServiceSubgroupBC serviceSubgroupBC;
-
 	public ServiceGroup loadGroup(Integer id) {
 		return serviceGroupBC.load(id);
-	}
-
-	public ServiceSubgroup loadSubgroup(Integer id) {
-		return serviceSubgroupBC.load(id);
 	}
 
 }
