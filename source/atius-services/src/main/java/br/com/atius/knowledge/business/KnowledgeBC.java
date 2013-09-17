e4d5a57452490c5aaa76591f52e8811754c50688
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2013-09-17 12:52:33 -0300

    new user interfaces to link Knowledge with ServiceItem

--- source/atius-services/src/main/java/br/com/atius/knowledge/business/KnowledgeBC.java
+++ source/atius-services/src/main/java/br/com/atius/knowledge/business/KnowledgeBC.java
@@ -1,5 +1,9 @@
 package br.com.atius.knowledge.business;
 
+import javax.inject.Inject;
+
+import br.com.atius.catalog.business.ServiceItemBC;
+import br.com.atius.catalog.domain.ServiceItem;
 import br.com.atius.knowledge.domain.Knowledge;
 import br.com.atius.knowledge.persistence.KnowledgeDAO;
 import br.gov.frameworkdemoiselle.stereotype.BusinessController;
@@ -9,5 +13,12 @@ import br.gov.frameworkdemoiselle.template.DelegateCrud;
 public class KnowledgeBC extends DelegateCrud<Knowledge, Integer, KnowledgeDAO> {
 
 	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private ServiceItemBC serviceItemBC;
+
+	public ServiceItem loadService(Integer id) {
+		return serviceItemBC.load(id);
+	}
 
 }
