69a7854d49528e2b8b561deb9d3fd5ddcde6c9f7
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2013-09-26 17:33:50 -0300

    clear clipboard after copy or move service

--- source/atius-services/src/main/java/br/com/atius/catalog/view/clipboard/ServiceItemClipboardEditMB.java
+++ source/atius-services/src/main/java/br/com/atius/catalog/view/clipboard/ServiceItemClipboardEditMB.java
@@ -56,6 +56,7 @@ public class ServiceItemClipboardEditMB extends AbstractEditPageBean<ServiceItem
 			if (operation == Operation.COPY)
 				getBean().setId(null);
 			bc.update(getBean());
+			editBean();
 		} catch (RuntimeException e) {
 			e.printStackTrace();
 			Faces.validationFailed();
@@ -71,6 +72,7 @@ public class ServiceItemClipboardEditMB extends AbstractEditPageBean<ServiceItem
 			if (operation == Operation.COPY)
 				getBean().setId(null);
 			bc.update(getBean());
+			editBean();
 		} catch (RuntimeException e) {
 			e.printStackTrace();
 			Faces.validationFailed();
