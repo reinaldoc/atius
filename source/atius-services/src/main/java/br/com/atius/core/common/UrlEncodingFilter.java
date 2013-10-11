1a643d668e3190ea217d586412ff62b4b5793cb1
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2013-10-11 13:24:07 -0300

    @WebFilter to set enconding to UTF-8

---
+++ source/atius-services/src/main/java/br/com/atius/core/common/UrlEncodingFilter.java
@@ -0,0 +1,33 @@
+package br.com.atius.core.common;
+
+import java.io.IOException;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(urlPatterns = { "/*" }, dispatcherTypes = { DispatcherType.FORWARD, DispatcherType.REQUEST })
+public class UrlEncodingFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+	}
+
+}
